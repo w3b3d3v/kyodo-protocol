eefc7e3e009d20fd311bee903e8c4883bdd2f5df
Yan Luiz Coito <nomadbitcoin@Yans-MacBook-Pro.local>
2023-08-31 14:48:43 -0300

    feat: managing routes when user have or not agreements

--- src/components/UserCheck/UserCheck.js
+++ src/components/UserCheck/UserCheck.js
@@ -1,68 +1,66 @@
 import React, { useState, useEffect } from 'react';
 import Web3 from 'web3';
-import UserRegistry from '../../contracts/UserRegistry.json';
 import { css } from "@emotion/react";
 import { BeatLoader } from "react-spinners";
-import SignInForm from '../SignInForm/SignInForm';
 
-const contractABI = UserRegistry.abi;
-const contractAddress = '0xd1be7ff959fE7Bc8E45F4432546C9D387C3836f4';
+import AgreementContract from '../../contracts/AgreementContract.json';
+import AddAgreement from "../AddAgreement/AddAgreement";
+import AgreementList from '../AgreementList/AgreementList';
+
+const contractABI = AgreementContract.abi;
+const contractAddress = '0x4C3073be445B97121ceE882D39299169fb22e1e5';
 
 const override = css`
   display: block;
   margin: 0 auto;
 `;
 
-//TODO: Colocar o css em outro arquivo e organizar a estilização;
-
 function UserCheck(props) {
     const [isRegistered, setIsRegistered] = useState(false);
     const [isLoading, setIsLoading] = useState(true);
   
     useEffect(() => {
-      async function checkUserRegistration() {
-        try {
-          // Verificar se o provedor Ethereum está presente
-          if (window.ethereum) {
-            // Conectar à blockchain usando o provedor Ethereum
-            const web3 = new Web3(window.ethereum);
+        async function checkUserRegistration() {
+            try {
+                // Verificar se o provedor Ethereum está presente
+                if (window.ethereum) {
+                    // Conectar à blockchain usando o provedor Ethereum
+                    const web3 = new Web3(window.ethereum);
   
-            // Criar uma instância do contrato usando o endereço e o ABI
-            const contract = new web3.eth.Contract(contractABI, contractAddress);
+                    // Criar uma instância do contrato usando o endereço e o ABI
+                    const contract = new web3.eth.Contract(contractABI, contractAddress);
   
-            // Verificar se o usuário está registrado
-            await contract.methods.getUser(props.userAddress).call();
-            setIsRegistered(true);
-          } else {
-            console.error('Provedor Ethereum não encontrado!');
-          }
-        } catch (error) {
-          if (error.message.includes('User not found')) {
-            setIsRegistered(false);
-          } else {
-            console.error(error);
-          }
-        } finally {
-          setIsLoading(false);
+                    // Verificar se o usuário possui acordos
+                    const userAgreementIds = await contract.methods.getUserAgreements(window.ethereum.selectedAddress).call();
+
+                    // Defina isRegistered como verdadeiro se o usuário tiver acordos
+                    setIsRegistered(userAgreementIds.length > 0);
+                } else {
+                    console.error('Provedor Ethereum não encontrado!');
+                }
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setIsLoading(false);
+            }
         }
-      }
   
-      checkUserRegistration();
-    }, [props.userAddress]);
+        checkUserRegistration();
+    }, []);
   
     if (isLoading) {
         return (
-          <div className="sweet-loading">
-            <BeatLoader color={"#36D7B7"} loading={isLoading} css={override} size={20} />
-          </div>
+            <div className="sweet-loading">
+                <BeatLoader color={"#36D7B7"} loading={isLoading} css={override} size={20} />
+            </div>
         );
     }
   
     if (isRegistered) {
-      return <>{props.privateRoute}</>;
+        return <AgreementList />;
+    } else {
+      return <AddAgreement />;
     }
-  
-    return <>{<SignInForm />}</>;
-  }
-  
-  export default UserCheck;  
+}
+
+export default UserCheck;
--- src/routes.js
+++ src/routes.js
@@ -1,53 +1,26 @@
 // routes.js
 import React from "react";
-import { Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import AddAgreement from "./components/AddAgreement/AddAgreement";
 import UserCheck from './components/UserCheck/UserCheck';
-import AgreementList from './components/AgreementList/AgreementList';
 
-//TODO: Props should not be agreementContract
-
-function AgreementsList(props) {
-  return (
-    <div>
-      <AgreementList agreementContract={props.account} />
-    </div>
-  );
-}
-
-const Layouts = (props) => {
+function Layouts(props) {
   return (
     <Routes>
       <Route
-        exact
         path="/"
-        element={
-          <UserCheck
-            userAddress={props.account}
-            privateRoute={<AgreementsList />}
-          />
-        }
+        element={<UserCheck userAddress={props.account} />}
       />
       <Route
         path="/agreementslist"
-        element={
-          <UserCheck
-            userAddress={props.account}
-            privateRoute={<AgreementsList />}
-          />
-        }
+        element={<UserCheck userAddress={props.account} />}
       />
       <Route
-        path="/addAgreement"
-        element={
-          <UserCheck
-            userAddress={props.account}
-            privateRoute={<AddAgreement />}
-          />
-        }
+        path="/addagreement"
+        element={<AddAgreement userAddress={props.account} />}
       />
     </Routes>
   );
-};
- 
+}
+
 export default Layouts;
