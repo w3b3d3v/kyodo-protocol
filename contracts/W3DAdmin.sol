958b736f03e5d27cc716596f6827b3041289e4e9
Yan Luiz Coito <nomadbitcoin@Yans-MacBook-Pro.local>
2023-09-06 19:42:37 -0300

    chore: unnecessary complexity for this version removed, the vault will only be for stablecoins

--- contracts/W3DAdmin.sol
+++ contracts/W3DAdmin.sol
@@ -6,13 +6,8 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
 contract W3DAdmin is AccessControl, Pausable {
-    bytes32 public constant CONTRACT_AUTH = keccak256("ContractCallPermisson");
-
     event NewAdminAdded(address indexed new_admin);
-    event NewAuthorizedContractAdded(address indexed new_authorized_contract);
-
     event RemovedAdmin(address indexed removed_admin);
-    event RemovedAuthorizedContract(address indexed removed_authorized_contract);
 
     constructor(address admin) {
         _setupRole(DEFAULT_ADMIN_ROLE, admin);
@@ -22,11 +17,6 @@ contract W3DAdmin is AccessControl, Pausable {
         require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Caller is not an Admin");
         _;
     }
-    
-    modifier onlyAuthorizedContract(){
-        require(hasRole(CONTRACT_AUTH, msg.sender), "Contract caller aren't authorized to make calls");
-        _;
-    } 
 
     function addAdmin(address account) external onlyAdmin() whenNotPaused() {
         _setupRole(DEFAULT_ADMIN_ROLE, account);
@@ -44,15 +34,5 @@ contract W3DAdmin is AccessControl, Pausable {
     
     function unpause() external onlyAdmin() whenPaused() {
         _unpause();
-    }
-
-    function addAuthorizedContract(address _contract) external onlyAdmin() whenNotPaused() {
-        _grantRole(CONTRACT_AUTH, _contract);
-        emit NewAuthorizedContractAdded(_contract);
-    }
-
-    function removeAuthorizedContract(address _contract) external onlyAdmin() whenNotPaused() {
-        _revokeRole(CONTRACT_AUTH, _contract);
-        emit RemovedAuthorizedContract(_contract);
     }
 }
