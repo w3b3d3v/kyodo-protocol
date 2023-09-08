cb4f8da7f4092b5e62080984efa8b2962268a43c
Mikael Carrara <mikaelmorattocarrara@gmail.com>
2023-09-08 10:59:23 -0300

    Added SASS files, variables and fonts. Styled footer and organized the styles in general

--- pages/index.js
+++ pages/index.js
@@ -18,14 +18,11 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
+          <Image src="/web3dev.svg" alt="WEB3DEV Logo" width={20} height={31} />
         </a>
       </footer>
     </div>
