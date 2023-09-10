9110f6e4580f97055fb08735c91eb3c69481a2ca
Yan Luiz Coito <nomadbitcoin@Yans-MacBook-Pro.local>
2023-09-09 22:00:07 -0300

    feat: dashboard added

--- components/dashboard/Balances.js
+++ components/dashboard/Balances.js
@@ -1,8 +1,41 @@
-import { useVaultContract } from "../ContractContext"
+import {useEffect, useState} from 'react';
+import { BeatLoader } from "react-spinners";
+import { useVaultContract } from "../../contexts/ContractContext"
+import { useAccount } from "../../contexts/AccountContext"
+
+import ERC20Token from '../../utils/ERC20Token';
+
+// TODO: Why the Vault balance of developer paid is 0 after be paid here but tests are running fine? Change the fakeStable to Vault 
 
 function Balances(props) {
-  // const { contract, loading } = useVaultContract();
-  // console.log("Balance of user: ",  contract)
+  const { contract, loading } = useVaultContract();
+  const { account } = useAccount();
+
+  useEffect(() => {
+    if (!loading) {
+      async function getUserBalance() {
+        try {
+          const myToken = new ERC20Token("0x34b179eCC554DE9bdBC9736E5E3E804e8318D8f3");
+          const balance = await myToken.getBalance(account);
+          console.log("balance", balance)
+        } catch (error) {
+          console.error(error);
+        }
+      }
+  
+      getUserBalance();
+    }
+  }, [loading]); 
+
+  if (loading) {
+      return (
+        <div className="loading-overlay">
+          <div className="sweet-loading">
+            <BeatLoader loading={loading} size={50} />
+          </div>
+        </div>
+      );
+  }
 
   return (
     <div>
