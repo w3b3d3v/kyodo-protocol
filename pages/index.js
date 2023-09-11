15286782315918526ca02d1572976087874613cc
Mikael Carrara <mikaelmorattocarrara@gmail.com>
2023-09-11 11:11:34 -0300

    Removed unused CSS

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 import UserCheck from "../components/UserCheck/UserCheck"
 
 export default function Home() {
