20d7b790372f7aa943bc9a8bda719c0fc6601930
Yan Luiz Coito <nomadbitcoin@Yans-MacBook-Pro.local>
2023-09-19 15:34:50 -0300

    feat: listing only 2 payments in list, click more button to see all payments in the payments page

---
+++ components/dashboard/Payments.js
@@ -0,0 +1,86 @@
+import { useEffect, useState } from 'react';
+import { BeatLoader } from "react-spinners";
+import { ethers } from "ethers";
+import { useAgreementContract } from "../../contexts/ContractContext";
+import { useAccount } from "../../contexts/AccountContext";
+import styles from "./Dashboard.module.css"
+
+function Payments ({ limit }) {
+    const { contract, loading } = useAgreementContract();
+    const { account } = useAccount();
+    const [paidAgreements, setPaidAgreements] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    
+    useEffect(() => {
+        setIsLoading(true);
+        if (!loading) {
+            fetchPaidAgreements();
+            setIsLoading(false);
+        }
+    }, [loading]);
+
+    if (paidAgreements.length === 0) {
+        return null;
+    }
+
+    async function fetchPaidAgreements() {
+        console.log("paidAgreements",paidAgreements)
+        const companyFilter = contract.filters.PaymentMade(account, null);
+        const professionalFilter = contract.filters.PaymentMade(null, account);
+
+        const companyAgreements = await contract.queryFilter(companyFilter);
+        const professionalAgreements = await contract.queryFilter(professionalFilter);
+
+        const allAgreements = [...companyAgreements, ...professionalAgreements];
+
+        setPaidAgreements(allAgreements.map(event => ({
+        ...event.args,
+        transactionHash: event.transactionHash
+        })));
+    }
+
+    function renderPaidAgreements() {
+        const displayedAgreements = limit ? paidAgreements.slice(0, limit) : paidAgreements;
+
+        return displayedAgreements.map((agreement, index) => (
+        <div key={index} className={styles["card"]}>
+            <h2>Agreement ID: {agreement.agreementId.toString()}</h2>
+            <p>
+            <strong>Status:</strong> {account.trim().toLowerCase() === agreement.company.trim().toLowerCase() ? "Paid" : "Received"}
+            </p>
+            <p>
+            <strong>Amount:</strong> {parseFloat(ethers.utils.formatUnits(agreement.amount, 18)).toFixed(2).replace(/\.00$/, '')} USD
+            </p>
+            <div className={styles["card-footer"]}>
+            <a href={`https://polygonscan.com/tx/${agreement.transactionHash}`} target="_blank" rel="noopener noreferrer" className={styles["confirm-btn"]}>
+                View on Polygonscan
+            </a>
+            </div>
+        </div>
+        ));
+    }
+
+    if (isLoading) {
+        return (
+          <div className={"loading-overlay"}>
+            <div className={"sweet-loading"}>
+              <BeatLoader loading={isLoading} size={50} />
+            </div>
+          </div>
+        )
+      }
+    
+    return (
+        <div className={styles["balance-list"]}>
+            <h1>Payments</h1>
+            <div className={styles["card-list"]}>
+                {renderPaidAgreements()}
+                {limit && paidAgreements.length > limit && (
+                    <button onClick={() => window.location.href='/payments'}>See More</button>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default Payments;
