78937e2a2c2d60de5cd07d9a6a4295dbe6717cd4
Yan Luiz Coito <nomadbitcoin@Yans-MacBook-Pro.local>
2023-09-20 14:29:10 -0300

    chore: chain module selector created

---
+++ chains/ContractManager.js
@@ -0,0 +1,30 @@
+import { 
+  initializeAgreementContract as initializeEthereumAgreementContract 
+} from './ethereum/initializeAgreementContract';
+import { 
+  initializeVaultContract as initializeEthereumVaultContract 
+} from './ethereum/initializeVaultContract';
+
+class ContractManager {
+  constructor() {
+    this.chainInitializers = {
+      ethereum: {
+        AgreementContract: initializeEthereumAgreementContract,
+        VaultContract: initializeEthereumVaultContract,
+      },
+      // solana: {
+      //   AgreementContract: initializeSolanaAgreementContract,
+      // },
+    };
+  }
+
+  getContract(chain, contractType) {
+    if (this.chainInitializers[chain] && this.chainInitializers[chain][contractType]) {
+      return this.chainInitializers[chain][contractType]();
+    }
+    throw new Error(`Unsupported chain or contract type: ${chain}, ${contractType}`);
+  }
+}
+
+const manager = new ContractManager();
+export default manager;
