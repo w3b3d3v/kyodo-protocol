a5cd3ffb6d68d28c839e6a48e579aac51868092e
Mikael Carrara <mikaelmorattocarrara@gmail.com>
2023-09-21 16:06:01 -0300

    styled: added progressbar to complete your profile call in the logged home

--- pages/index.js
+++ pages/index.js
@@ -14,15 +14,21 @@ export default function Home() {
           <ul className="home-calls">
             <li>
               <h2>Complete your profile to be visible</h2>
+              <div className="progressbar">
+                <div></div>
+              </div>
               <p>You profile is <strong>35%</strong> complete</p>
+              <a href="#">Complete profile</a>
             </li>
             <li>
               <h2>Validate your experience</h2>
               <p>With ZKP you can prove your skills and experience minting a NFT to display it in your profile.</p>
+              <a href="#">Start validating</a>
             </li>
             <li>
-              <h2>Refer and earn</h2>
+              <h2>Refer and<br></br> earn</h2>
               <p>Professionals or contractors that refer the usage of Kyodo, can earn a % of paid value to the protocol.</p>
+              <a href="#">Get referral link</a>
             </li>
           </ul>
         </section>
