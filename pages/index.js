e7c8a789d97d93b92eedbe3b9338437c974244a9
Mikael Carrara <mikaelmorattocarrara@gmail.com>
2023-09-21 15:38:11 -0300

    style: added some styled to logged home

--- pages/index.js
+++ pages/index.js
@@ -13,13 +13,16 @@ export default function Home() {
           <h1>Hi, Josy</h1>
           <ul className="home-calls">
             <li>
-              <p>Complete your profile to be visible</p>
+              <h2>Complete your profile to be visible</h2>
+              <p>You profile is <strong>35%</strong> complete</p>
             </li>
             <li>
-              <p>Validate your experience</p>
+              <h2>Validate your experience</h2>
+              <p>With ZKP you can prove your skills and experience minting a NFT to display it in your profile.</p>
             </li>
             <li>
-              <p>Refer and earn</p>
+              <h2>Refer and earn</h2>
+              <p>Professionals or contractors that refer the usage of Kyodo, can earn a % of paid value to the protocol.</p>
             </li>
           </ul>
         </section>
