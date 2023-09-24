32c2d111b15a58ae83b1102bce41dde0f74389a8
Yan Luiz Coito <nomadbitcoin@Yans-MacBook-Pro.local>
2023-09-23 23:10:23 -0300

    fix: selectedChain is not hardcoded anymore

--- chains/ContractManager.js
+++ chains/ContractManager.js
@@ -22,7 +22,9 @@ class ContractManager {
     if (this.chainInitializers[chain] && this.chainInitializers[chain][contractType]) {
       return this.chainInitializers[chain][contractType]();
     }
-    throw new Error(`Unsupported chain or contract type: ${chain}, ${contractType}`);
+    alert("Please select a valid chain")
+    localStorage.setItem('selectedChain', null);
+    // throw new Error(`Unsupported chain or contract type: ${chain}, ${contractType}`);
   }
 }
 
--- contexts/AccountContext.js
+++ contexts/AccountContext.js
@@ -1,76 +1,89 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { vefifyChain } from "../components/ConnectWalletButton/ConnectWalletButton"
+import { vefifyChain } from "../components/ConnectWalletButton/ConnectWalletButton";
 
 const AccountContext = createContext({
   account: null,
   setAccount: () => {},
   selectedChain: null,
-  setSelectedChain: () => {}
+  setSelectedChain: () => {},
 });
 
 export function useAccount() {
-    return useContext(AccountContext);
+  return useContext(AccountContext);
 }
 
 export function AccountProvider({ children }) {
-    const [account, setAccount] = useState(null);
-    const [selectedChain, setSelectedChain] = useState(() => {
-      if (typeof window !== 'undefined') {
-          return localStorage.getItem('selectedChain') || null;
-      }
-      return null;
-    });
-
-    const handleDisconnect = () => {
-        setAccount(null)
-        localStorage.setItem('selectedChain', null);
+  const [account, setAccount] = useState(null);
+  const [selectedChain, setSelectedChain] = useState(() => {
+    if (typeof window !== 'undefined') {
+      return localStorage.getItem('selectedChain') || null;
     }
+    return null;
+  });
 
-    useEffect(() => {
-      if (typeof window !== 'undefined' && selectedChain) {
-          localStorage.setItem('selectedChain', selectedChain);
-      }
-    }, [selectedChain]);
+  const handleDisconnect = () => {
+    setAccount(null);
+    localStorage.setItem('selectedChain', null);
+  };
 
-    const updateAccount = async () => {
-      if (selectedChain === 'ethereum' && window.ethereum){
-        try {
-          await window.ethereum.request({ method: "eth_requestAccounts" });
-          const provider = new ethers.providers.Web3Provider(window.ethereum);
-          const accounts = await provider.listAccounts();
-          await vefifyChain()
+  useEffect(() => {
+    if (typeof window !== 'undefined' && selectedChain) {
+      localStorage.setItem('selectedChain', selectedChain);
+    }
+  }, [selectedChain]);
+
+  const updateAccount = async () => {
+    if (selectedChain === 'ethereum' && window.ethereum) {
+      try {
+        const accounts = await window.ethereum.request({ method: "eth_accounts" });
+        if (accounts.length > 0 && accounts[0] !== account) {
+          await vefifyChain();
           setAccount(accounts[0]);
-        } catch (error) {
-          console.error(error);
         }
-      }
-        
-      if (selectedChain === 'solana' && window.solana) {        
-        try {
-          await window.solana.connect();
-          const solanaAccount = window.solana.publicKey.toString();
-          // TODO: Solana: verify connected chain for development, staging, or production.
-          setAccount(solanaAccount);
-        } catch (error) {
-          console.error("Erro ao conectar com a Phantom Wallet:", error);
-        }
+      } catch (error) {
+        console.error(error);
       }
     }
 
-    useEffect(() => {
-        updateAccount()
-        window.ethereum.on("accountsChanged", updateAccount)
-        window.ethereum.on("disconnect", handleDisconnect)
+    if (selectedChain === 'solana' && window.solana) {
+      try {
+        await window.solana.connect();
+        const solanaAccount = window.solana.publicKey.toString();
+        // TODO: Solana: verify connected chain for development, staging, or production.
+        setAccount(solanaAccount);
+      } catch (error) {
+        console.error("Erro ao conectar com a Phantom Wallet:", error);
+      }
+    }
+  };
 
-        return () => {
-        window.ethereum.removeListener("accountsChanged", updateAccount)
-        window.ethereum.removeListener("disconnect", handleDisconnect)
-        }
-    }, [account])
+  useEffect(() => {
+    updateAccount();
 
-    return (
-      <AccountContext.Provider value={{ account, setAccount, selectedChain, setSelectedChain }}>
-            {children}
-        </AccountContext.Provider>
-    );
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", updateAccount);
+      window.ethereum.on("disconnect", handleDisconnect);
+
+      return () => {
+        window.ethereum.removeListener("accountsChanged", updateAccount);
+        window.ethereum.removeListener("disconnect", handleDisconnect);
+      };
+    }
+
+    if (window.solana) {
+      window.solana.on("connect", updateAccount);
+      window.solana.on("disconnect", handleDisconnect);
+
+      return () => {
+        window.solana.removeListener("connect", updateAccount);
+        window.solana.removeListener("disconnect", handleDisconnect);
+      };
+    }
+  }, [account, selectedChain]);
+
+  return (
+    <AccountContext.Provider value={{ account, setAccount, selectedChain, setSelectedChain }}>
+      {children}
+    </AccountContext.Provider>
+  );
 }
