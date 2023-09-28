ff2900a8ea7bdefd287c7d79d3de8ba01b6c7653
Bruno Sanguinetti <brunoebarros@gmail.com>
2023-09-28 14:08:22 -0300

    feat: init fake payment token + associated token account

--- contracts/solana/tests/kyodo-protocol-sol.ts
+++ contracts/solana/tests/kyodo-protocol-sol.ts
@@ -2,13 +2,14 @@ import * as anchor from "@coral-xyz/anchor";
 import { BN, Program } from "@coral-xyz/anchor";
 import { AgreementProgram } from "../target/types/agreement_program";
 import {  } from "bn.js";
-import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID, mintTo, getOrCreateAssociatedTokenAccount, createMint } from "@solana/spl-token";
+import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
 
 describe("agreement_program", () => {
   // Configure the client to use the local cluster.
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
-
+  
   const program = anchor.workspace.AgreementProgram;
   const companyAddress = provider.wallet.publicKey;
   const professionalAddress = anchor.web3.Keypair.generate();
@@ -17,19 +18,54 @@ describe("agreement_program", () => {
   const fakeMint = anchor.web3.Keypair.generate()
 
   it("Initializes a fake payment token", async () => {
-
-    const tx = await program.methods.initializeMint().accounts({
-      mint: fakeMint.publicKey,
-      payer: companyAddress,
-      systemProgram: anchor.web3.SystemProgram.programId,
-      tokenProgram: TOKEN_PROGRAM_ID,
-      rent: anchor.web3.SYSVAR_RENT_PUBKEY
-     }).signers([fakeMint]).rpc();
+    const payer = (provider.wallet as NodeWallet).payer
+    const tx = await createMint(
+      provider.connection,
+      payer,
+      companyAddress,
+      companyAddress, 
+      8,
+      fakeMint,
+      null,
+      TOKEN_PROGRAM_ID,
+    );
 
      console.log("Your token address:", fakeMint.publicKey);
      console.log("Your transaction signature:", tx);
 
    });
+
+   it("Creates Associated Token Account and Mints Tokens", async () => {
+    const payer = (provider.wallet as NodeWallet).payer
+    // Create the associated token account for companyAddress
+    const associatedTokenAddress = await getOrCreateAssociatedTokenAccount(
+      provider.connection,
+      payer,
+      fakeMint.publicKey,
+      companyAddress,
+      false,
+      null,
+      null,
+      TOKEN_PROGRAM_ID,
+      ASSOCIATED_TOKEN_PROGRAM_ID,
+    );
+
+  
+    // Mint to the associated token address
+    await mintTo(
+      provider.connection,
+      payer, 
+      fakeMint.publicKey,
+      associatedTokenAddress.address,
+      companyAddress, 
+      10000,
+      [],
+      null,
+      TOKEN_PROGRAM_ID,
+    );
+    
+    console.log("Tokens minted to associated token account:", associatedTokenAddress.toString());
+  });
 
   it("Initialize first Agreement", async () => {
     const stingBuffer = Buffer.from("company_agreements", "utf-8");
