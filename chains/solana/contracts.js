c1b555fc5865c144b8cb6a7da4a4824f04edf43a
Yan Luiz Coito <nomadbitcoin@Yans-MacBook-Pro.local>
2023-10-03 18:05:05 -0300

    feat: agreementContract being initialized for solana

--- chains/solana/contracts.js
+++ chains/solana/contracts.js
@@ -1,3 +1,11 @@
-const contracts = {}
+import { agreementContract } from "./agreementContract"
+// import { vaultContract } from "./contracts/vaultContract"
 
+async function verify() {
+  // Unfortunatelly phantom wallet or solana object does not offer a clear way to get the network id
+  // so we have to use just ask the user to manually change to the localnetwork while testing
+  alert("Please change to localnet. Phantom > Settings > Developer Settings")
+}
+
+const contracts = { agreementContract, verify }
 export default contracts
