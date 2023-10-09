de276c254f52192fbb41ee4c7ba638b995a8b4e2
Mikael Carrara <mikaelmorattocarrara@gmail.com>
2023-10-09 15:25:39 -0300

    feat: added translate and created community form page

--- pages/onboarding/index.js
+++ pages/onboarding/index.js
@@ -1,12 +1,14 @@
-//import OnboardingProfileSelection from '../../components/Onboarding/OnboardingProfileSelection.js';
+import OnboardingProfileSelection from '../../components/Onboarding/OnboardingProfileSelection.js';
+import OnboardingCommunity from '../../components/Onboarding/OnboardingCommunity.js';
 import OnboardingProfessional from '../../components/Onboarding/OnboardingProfessional.js';
 import OnboardingContractor from '../../components/Onboarding/OnboardingContractor.js';
 
 function Onboarding() {
   return (
     //<OnboardingProfileSelection />
+    <OnboardingCommunity />
     //<OnboardingProfessional />
-    <OnboardingContractor />
+    //<OnboardingContractor />
   )
 }
   
