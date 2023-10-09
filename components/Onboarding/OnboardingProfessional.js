d821ca613520c496aa4a2661a00a037897b93d35
Mikael Carrara <mikaelmorattocarrara@gmail.com>
2023-10-09 08:26:37 -0300

    feat: added onboarding basic structure

---
+++ components/Onboarding/OnboardingProfessional.js
@@ -0,0 +1,14 @@
+import { useAccount } from "../../contexts/AccountContext";
+import styles from "./Onboarding.module.scss"
+import Image from 'next/image'
+
+function OnboardingProfessional() {
+
+  return (
+    <div className={styles["onboarding"]}>
+
+    </div>
+  )
+}
+
+export default OnboardingProfessional;
