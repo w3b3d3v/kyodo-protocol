5bcf2d9b8e26c21853fe6a35c06b41dc996c2eaf
Mikael Carrara <mikaelmorattocarrara@gmail.com>
2023-10-09 08:26:37 -0300

    feat: added onboarding basic structure

---
+++ components/Onboarding/OnboardingProfileSelection.js
@@ -0,0 +1,14 @@
+import { useAccount } from "../../contexts/AccountContext";
+import styles from "./Onboarding.module.scss"
+import Image from 'next/image'
+
+function OnboardingProfileSelection() {
+
+  return (
+    <div className={styles["profile-selection"]}>
+
+    </div>
+  )
+}
+
+export default OnboardingProfileSelection;
---
+++ pages/onboarding/index.js
@@ -0,0 +1,9 @@
+import OnboardingProfessional from '../../components/Onboarding/OnboardingProfessional.js';
+
+function Onboarding() {
+  return (
+    <OnboardingProfessional />
+  )
+}
+  
+export default Onboarding;
