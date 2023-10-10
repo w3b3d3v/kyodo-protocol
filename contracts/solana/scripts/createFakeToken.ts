a36fe950bb03539e3fcd7b25f91625c870f1826b
Yan Luiz Coito <nomadbitcoin@Yans-MacBook-Pro.local>
2023-10-10 13:12:04 -0300

    feat: script to create fakeStable for solana

--- contracts/solana/scripts/createFakeToken.ts
+++ contracts/solana/scripts/createFakeToken.ts
@@ -1,7 +1,8 @@
 import * as anchor from "@coral-xyz/anchor";
 import * as dotenv from "dotenv";
-import path from "path";
-dotenv.config({ path: path.resolve(__dirname, '../../../.env.development.local') });
+const fs = require("fs");
+const path = require("path");
+import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet"; // NodeWallet is a class from Anchor that represents a wallet on a Solana node.
 import { 
   TOKEN_PROGRAM_ID, 
   ASSOCIATED_TOKEN_PROGRAM_ID, 
@@ -9,25 +10,46 @@ import {
   getOrCreateAssociatedTokenAccount, 
   createMint 
 } from "@solana/spl-token"; // SPL token utilities for token creation, minting, and account association.
-import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet"; // NodeWallet is a class from Anchor that represents a wallet on a Solana node.
+dotenv.config({ path: path.resolve(__dirname, '../../../.env.development.local') });
 
+
+function updateConfig(fakeStableAddress) {
+  const envPath = path.join(__dirname, '../../../.env.development.local');
+  let envData = fs.readFileSync(envPath, 'utf8');
+  const lines = envData.split('\n');
+
+  const keysToUpdate = {
+    'NEXT_PUBLIC_SOLANA_FAKE_STABLE_ADDRESS': fakeStableAddress
+  };
+
+  Object.keys(keysToUpdate).forEach(key => {
+    let found = false;
+    for (let i = 0; i < lines.length; i++) {
+      if (lines[i].startsWith(`${key}=`)) {
+        lines[i] = `${key}=${keysToUpdate[key]}`;
+        found = true;
+        break;
+      }
+    }
+    if (!found) {
+      lines.push(`${key}=${keysToUpdate[key]}`);
+    }
+  });
+
+  envData = lines.join('\n');
+  fs.writeFileSync(envPath, envData);
+  console.log(`Updated fakeStable for Solana addresses in ${envPath}`);
+}
 
 async function createFakeToken() {
   try {
     // Configure the client to use the local Solana cluster.
-    const provider = anchor.AnchorProvider.local("http://localhost:8899");
+    const provider = anchor.AnchorProvider.local("https://api.devnet.solana.com");
     anchor.setProvider(provider);
 
     // Fetch the public key of the company from the provider's wallet.
     const companyAddress = provider.wallet.publicKey;
-
-    // Generate a new keypair for the professional's address.
-    // TODO: Save professionalAddress to local env
-    // to use in the
-    const professionalAddress = anchor.web3.Keypair.generate();
-    
-    console.log("professionalAddress", professionalAddress.publicKey.toBase58());
-    console.log("companyAddress", companyAddress.toBase58());
+    console.log("companyAddress", companyAddress.toBase58())
 
     const payer = (provider.wallet as NodeWallet).payer;
     const fakeMint = anchor.web3.Keypair.generate();
@@ -45,13 +67,12 @@ async function createFakeToken() {
     );
 
     // Logging the minted token's address and the transaction signature.
-    console.log("Your token address:", fakeMint.publicKey);
-    console.log("Your transaction signature:", txMint);
+    // console.log("Your token address:", fakeMint.publicKey);
+    // console.log("Your transaction signature:", txMint);
     // Defining the agreement details.
 
     // Fetching the payer's account which will fund the transactions.
     let associatedTokenAddressCompany; // Will be asign, and then used to pay the professional
-    let associatedTokenAddressProfessional; // Will be asign, and then used to recieve payment from the company
     //let toPayAgreementAddress; // Will be asign, and then updated when the paymen is processed
 
     // Create or fetch the associated token account for the company using the fake mint.
@@ -67,18 +88,6 @@ async function createFakeToken() {
     );
 
     console.log(associatedTokenAddressCompany);
-
-    // Creating or fetching the associated token account for the professional.
-    associatedTokenAddressProfessional = await getOrCreateAssociatedTokenAccount(
-      provider.connection,                // Current provider's connection.
-      payer,                              // Entity funding the transaction.
-      fakeMint.publicKey,                 // Mint's public key.
-      professionalAddress.publicKey,      // Owner of the associated token account.
-      false,                              // Indicates if the function should throw an error if the account already exists.
-      null, null,                         // Optional configs (multiSig and options).
-      TOKEN_PROGRAM_ID,                   // SPL token program ID.
-      ASSOCIATED_TOKEN_PROGRAM_ID         // SPL associated token program ID.
-    );
 
     // Mint tokens to the company's associated token account.
     var mintToCompanyTx = await mintTo(
@@ -96,6 +105,9 @@ async function createFakeToken() {
     console.log("Minted successfully");
     console.log("Fake Token address:", fakeMint.publicKey.toBase58());
     console.log("Transaction signature:", mintToCompanyTx);
+
+    updateConfig(fakeMint.publicKey.toBase58());
+
   } catch (error) {
     console.error("Error minting:", error);
   }
--- contracts/solana/scripts/deploy.sh
+++ contracts/solana/scripts/deploy.sh
@@ -41,6 +41,11 @@ if [ "$env" == "devnet" ]; then
         echo "NEXT_PUBLIC_SOLANA_AGREEMENT_CONTRACT_ADDRESS=$PROGRAM_ID" >> "$ENV_FILE"
     fi
 
+    # Deploy the fake Stablecoin
+    echo "Creating fake token..."
+    DEPLOY_FAKE_STABLE_OUTPUT=$(ts-node $DIR/createFakeToken.ts)
+    echo "$DEPLOY_FAKE_STABLE_OUTPUT"
+
 elif [ "$env" == "testnet" ]; then
   echo "Deploying to testnet..."
     # Get the directory of the script
