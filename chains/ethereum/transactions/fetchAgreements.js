422e1312e02a700ef87ec943927c1cf2387e3eec
Yan Luiz Coito <nomadbitcoin@Yans-MacBook-Pro.local>
2023-10-11 21:03:46 -0300

    chore: amounts being displayed with correct lamports/wei

--- chains/ethereum/transactions/fetchAgreements.js
+++ chains/ethereum/transactions/fetchAgreements.js
@@ -1,21 +1,56 @@
+import { ethers } from "ethers";
+
+function weiToEther(weiValue) {
+  const ether = ethers.utils.formatEther(weiValue);
+  return Math.round(ether).toString();
+}
+
+function transformAgreementData(agreement) {
+  // console.log("agreement data", agreement)
+  // console.log("agreement", agreement.id.toString())
+  // console.log("title",agreement.title)
+  // console.log("description",agreement.description)
+  // console.log("professional",agreement.professional)
+  // console.log("skills",agreement.skills)
+  // console.log("payment",agreement.payment.toString())
+  // console.log("totalPaid",agreement.totalPaid)
+
+  return {
+      id: agreement.id, 
+      title: agreement.title,
+      description: agreement.description,
+      professional: agreement.professional?.toString(),
+      company: agreement.company?.toString(),
+      skills: agreement.skills,
+      amount: ethers.utils.formatUnits(agreement.payment.amount, 18),
+      totalPaid: ethers.utils.formatUnits(agreement.totalPaid, 18)
+  };
+}
+
+
 export const fetchAgreements = async (details) => {
-    try {
-      const userAgreementIds = await details.contract.getUserAgreements(details.account)
-      if (userAgreementIds.length === 0) return null;
+  try {
+    const userAgreementIds = await details.contract.getUserAgreements(details.account);
+    
+    if (userAgreementIds.length === 0) return null;
 
-      const stringIds = userAgreementIds.map((id) => id.toString())
+    const stringIds = userAgreementIds.map((id) => id.toString());
 
-      const fetchedAgreements = await Promise.all(
-        stringIds.map(async (agreementId) => {
-          const agreement = await details.contract.getAgreementById(agreementId)
-          return agreement
-        })
-      )
+    const fetchedAgreements = await Promise.all(
+      stringIds.map(async (agreementId) => {
+        const agreement = await details.contract.getAgreementById(agreementId);
+        const transformedAgreement = transformAgreementData(agreement);
+        return {
+            ...transformedAgreement
+        };
+      })
+    );
 
-      return fetchedAgreements
-    } catch (error) {
-      console.error("Error when fetching agreements:", error)
-    }
+    return fetchedAgreements;
+  } catch (error) {
+    console.error("Error when fetching agreements:", error);
   }
+};
+
 
 export default fetchAgreements;
--- chains/solana/contracts.js
+++ chains/solana/contracts.js
@@ -4,7 +4,7 @@ import { vaultContract } from "./contracts/vaultContract"
 async function verify() {
   // Unfortunatelly phantom wallet or solana object does not offer a clear way to get the network id
   // so we have to use just ask the user to manually change to the localnetwork while testing
-  alert("Please change to localnet. Phantom > Settings > Developer Settings")
+  alert("Please change to Devnet. Phantom > Settings > Developer Settings")
 }
 
 const contracts = { agreementContract, vaultContract, verify }
