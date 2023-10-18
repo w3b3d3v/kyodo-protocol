4c80ff3238bd01b4ae92781c2d19a36db95231d4
Yan Luiz Coito <nomadbitcoin@Yans-MacBook-Pro.local>
2023-10-18 13:22:13 -0300

    fix: corrected variable name, tested on Mumbai

--- contracts/ethereum/hardhat.config.js
+++ contracts/ethereum/hardhat.config.js
@@ -26,6 +26,11 @@ let config = {
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com/" || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
   },
   contractSizer: {
     alphaSort: false,
