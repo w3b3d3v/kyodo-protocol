41db52408b3f059fcb7badf9b786e87b400632f5
Yan Luiz Coito <nomadbitcoin@Yans-MacBook-Pro.local>
2023-10-18 13:00:32 -0300

    fix: in real networks we can't filter events from the Block zero

--- chains/ethereum/transactions/fetchPaidAgreements.js
+++ chains/ethereum/transactions/fetchPaidAgreements.js
@@ -2,14 +2,19 @@ import { ethers } from "ethers";
 
 export const fetchPaidAgreements = async (details) => {
   try {
+    // Define the starting block
+    const startBlock = parseInt(process.env.NEXT_PUBLIC_DEPLOYMENT_BLOCK);
+    console.log('latestB whatlock', process.env.NEXT_PUBLIC_DEPLOYMENT_BLOCK)
+    const latestBlock = await details.contract.provider.getBlockNumber(); // get the latest block number
+    
     const companyFilter = details.contract.filters.PaymentMade(details.account, null);
     const professionalFilter = details.contract.filters.PaymentMade(null, details.account);
-  
-    const companyAgreements = await details.contract.queryFilter(companyFilter);
-    const professionalAgreements = await details.contract.queryFilter(professionalFilter);
-  
+
+    const companyAgreements = await details.contract.queryFilter(companyFilter, startBlock, latestBlock);
+    const professionalAgreements = await details.contract.queryFilter(professionalFilter, startBlock, latestBlock);
+
     const allAgreements = [...companyAgreements, ...professionalAgreements];
-  
+
     const agreements = allAgreements.map(event => {
       let formattedAmount = ethers.utils.formatUnits(event.args.amount, 18); //TODO: get the correct amount of decimals based on the token
       return {
@@ -18,12 +23,12 @@ export const fetchPaidAgreements = async (details) => {
         transactionHash: event.transactionHash
       };
     });
-  
+
     return agreements;
   } catch (error) {
-    console.log("error: ", error)
+    console.log("error: ", error);
     throw new Error("Error in fetching agreements: ", error);
-  }  
-};
+  }
+}
   
 export default fetchPaidAgreements;
