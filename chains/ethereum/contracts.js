26398471116531931e7515248884adc5b92db16c
Yan Luiz Coito <nomadbitcoin@Yans-MacBook-Pro.local>
2023-10-24 21:43:15 -0300

    feat: getting addresses from Registry, contract instances optimized

--- chains/ethereum/contracts.js
+++ chains/ethereum/contracts.js
@@ -1,32 +1,64 @@
-import { agreementContract } from "./contracts/agreementContract"
-import { vaultContract } from "./contracts/vaultContract"
+import { ethers } from 'ethers';
+import KyodoRegistry from './abis/KyodoRegistry.json';
+import AgreementContract from './abis/AgreementContract.json';
+import VaultContract from './abis/StableVault.json';
 
-async function verify() {
-  const TEST_NETWORKS = ["0x7A69", "31337", "534351"]
-  const testEnv = process.env.NODE_ENV === "development"
-  const chainId = window.ethereum.networkVersion
+const REGISTRY_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_KYODO_REGISTRY
 
-  if (testEnv) {
-    if (!TEST_NETWORKS.includes(chainId)) {
-      await window.ethereum.request({
-        method: "wallet_addEthereumChain",
-        params: [
-          {
-            chainId: TEST_NETWORKS[0],
-            rpcUrls: ["http://localhost:8545"],
-            chainName: "Hardhat",
-            nativeCurrency: {
-              name: "ETH",
-              symbol: "HETH",
-              decimals: 18,
-            },
-          },
-        ],
-      })
-    }
+function getRegistryContract() {
+  try {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const registryContractInstance = new ethers.Contract(REGISTRY_CONTRACT_ADDRESS, KyodoRegistry.abi, provider.getSigner());
+    return registryContractInstance;
+  } catch (e) {
+    console.error("Failed to instantiate the registry contract", e);
   }
 }
 
-const contracts = { agreementContract, vaultContract, verify }
+async function getContractInstance(abi, contractName) {
+  try {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const registryContract = getRegistryContract();
 
-export default contracts
+    const contractAddress = await registryContract.getRegistry(contractName);
+
+    return new ethers.Contract(contractAddress, abi, provider.getSigner()); 
+  } catch (e) {
+    console.log("failed to instantiate contract")
+    console.log(e)
+  }
+}
+
+const contracts = {
+  kyodoRegistry: getRegistryContract,
+  agreementContract: () => getContractInstance(AgreementContract.abi, "AGREEMENT_CONTRACT_ADDRESS"),
+  vaultContract: () => getContractInstance(VaultContract.abi, "VAULT_CONTRACT_ADDRESS"),
+  
+  verify: async () => {
+    const TEST_NETWORKS = ["0x7A69", "31337", "534351"]
+    const testEnv = process.env.NODE_ENV === "development"
+    const chainId = window.ethereum.networkVersion
+  
+    if (testEnv) {
+      if (!TEST_NETWORKS.includes(chainId)) {
+        await window.ethereum.request({
+          method: "wallet_addEthereumChain",
+          params: [
+            {
+              chainId: TEST_NETWORKS[0],
+              rpcUrls: ["http://localhost:8545"],
+              chainName: "Hardhat",
+              nativeCurrency: {
+                name: "ETH",
+                symbol: "HETH",
+                decimals: 18,
+              },
+            },
+          ],
+        })
+      }
+    }
+  },
+};
+
+export default contracts;
