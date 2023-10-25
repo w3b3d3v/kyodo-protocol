53163a9706dfda083dc57b237674465592a16ada
Yan Luiz Coito <nomadbitcoin@Yans-MacBook-Pro.local>
2023-10-25 17:16:41 -0300

    chore: contractContext other unused components removed

--- pages/payments/index.js
+++ pages/payments/index.js
@@ -1,10 +1,7 @@
-import { AgreementContractProvider } from "../../contexts/ContractContext";
 import Payments from "../../components/Dashboard/Payments.js"
 
 export default function Agreements() {
   return (
-    <AgreementContractProvider>
       <Payments />
-    </AgreementContractProvider>
   )
 }
