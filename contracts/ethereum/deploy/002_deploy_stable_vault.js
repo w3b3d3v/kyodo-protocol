5ce01823f13f041662528890e3b8aa5ca89bcaaf
Lucas Archangelo <lustreet@gmail.com>
2023-11-22 11:38:23 -0300

    build: including build and config files

---
+++ contracts/ethereum/deploy/002_deploy_stable_vault.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const chainConfigs = require('../scripts/utils/chain_config');
+
+module.exports = async ({getNamedAccounts, deployments}) => {
+  const {deploy} = deployments;
+  const {deployer} = await getNamedAccounts();
+  console.log(`Deployer: ${deployer}`);
+  
+  const salt = '0x';
+
+  const tokenName = "Kyodo Token";
+  const tokenSymbol = "KYO";
+  const networkName = hre.network.name;
+  const {routerAddress} = chainConfigs[networkName];
+
+  const deployedContract = await deploy('StableVault', {
+    from: deployer,
+    args: [deployer, tokenName, tokenSymbol, routerAddress],
+    log: true,
+    deterministicDeployment: salt
+  });
+
+//   const envPath = path.join(__dirname, '../../../.env.development.local');
+//   const envContent = fs.readFileSync(envPath, { encoding: 'utf8' });
+//   const newEnvContent = envContent.replace(/NEXT_PUBLIC_KYODO_REGISTRY=.*/, `NEXT_PUBLIC_KYODO_REGISTRY=${deployedContract.address}`);
+//   fs.writeFileSync(envPath, newEnvContent);
+//   console.log(`Updated NEXT_PUBLIC_KYODO_REGISTRY in env.development.local to ${deployedContract.address}`);
+};
+
+module.exports.tags = ['StableVault'];
---
+++ contracts/ethereum/deploy/003_deploy_agreement_contract.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const {chainConfigs, chainIdList, chainSelectorList} = require('../scripts/utils/chain_config');
+
+module.exports = async ({getNamedAccounts, deployments, ethers, network}) => {
+  const {deploy} = deployments;
+  const {deployer, kyodoTreasury, communityTreasury} = await getNamedAccounts();
+  console.log(`Deployer: ${deployer}`);
+  
+  const salt = '0x';
+
+  const networkName = network.name;
+  const {linkAddress, routerAddress, chainSelector} = chainConfigs[networkName];
+
+  const stableVaultInstance = await ethers.getContract('StableVault');
+
+  const deployedContract = await deploy('AgreementContract', {
+    from: deployer,
+    args: [kyodoTreasury, communityTreasury, deployer, routerAddress, linkAddress, chainSelector, stableVaultInstance.address, chainIdList, chainSelectorList],
+    log: true,
+    deterministicDeployment: salt
+  });
+
+//   const envPath = path.join(__dirname, '../../../.env.development.local');
+//   const envContent = fs.readFileSync(envPath, { encoding: 'utf8' });
+//   const newEnvContent = envContent.replace(/NEXT_PUBLIC_KYODO_REGISTRY=.*/, `NEXT_PUBLIC_KYODO_REGISTRY=${deployedContract.address}`);
+//   fs.writeFileSync(envPath, newEnvContent);
+//   console.log(`Updated NEXT_PUBLIC_KYODO_REGISTRY in env.development.local to ${deployedContract.address}`);
+};
+
+module.exports.tags = ['AgreementContract'];
