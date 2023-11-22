b1fdf8718a90e56a909816673cae8b81796f17ce
Lucas Archangelo <lustreet@gmail.com>
2023-11-22 14:40:37 -0300

    test: implementing tests

--- contracts/ethereum/test/AgreementContract/AgreementContract_v2.test.js
+++ contracts/ethereum/test/AgreementContract/AgreementContract_v2.test.js
@@ -1,29 +1,58 @@
-const { ethers, deployments, getUnnamedAccounts, getNamedAccounts } = require("hardhat");
+const { ethers, deployments, getNamedAccounts } = require("hardhat");
+const { expect, assert } = require("chai");
 
 const setup = deployments.createFixture(async () => {
     await deployments.fixture(['AgreementContract']);
     const { deployer, user1, user2 } = await getNamedAccounts();
+    const skills = [
+        { name: "Programming", level: 50 },
+        { name: "Design", level: 50 }
+    ];
 
     return {
         DeployerUser: {
             address: deployer,
             AgreementContract: await ethers.getContract('AgreementContract', deployer),
         },
-        User1: {
+        Company: {
             address: user1,
             AgreementContract: await ethers.getContract('AgreementContract', user1),
         },
-        User2: {
+        Employee: {
             address: user2,
             AgreementContract: await ethers.getContract('AgreementContract', user2),
         },
+        skills,
     }
 });
 
 describe('AgreementContract', () => {
-    it('testing 1 2 3', async function () {
-        const { DeployerUser, User1, User2 } = await setup();
-        // await DeployerUser.AgreementContract.addAcceptedPaymentToken("0x779877A7B0D9E8603169DdbD7836e478b4624789");
+    it('Add acceptedPaymentToken should only set using owner', async function () {
+        const { DeployerUser, Company } = await setup();
         await DeployerUser.AgreementContract.addAcceptedPaymentToken("0x779877A7B0D9E8603169DdbD7836e478b4624789");
+        let address = await DeployerUser.AgreementContract.getAcceptedPaymentTokens();
+        assert(address == "0x779877A7B0D9E8603169DdbD7836e478b4624789");
+        try {
+            await Company.AgreementContract.addAcceptedPaymentToken("0x779877A7B0D9E8603169DdbD7836e478b4624780");
+        } catch (err) {
+            assert(err);
+        }
+
+        address = await DeployerUser.AgreementContract.getAcceptedPaymentTokens();
+        assert(address == "0x779877A7B0D9E8603169DdbD7836e478b4624789");
+    });
+
+    it('Should create a new agreement with authorized tokens', async function () {
+        const { Company, Employee, skills } = await setup();
+        const paymentAmount = ethers.parseEther("5");
+        await Company.AgreementContract.createAgreement(
+            "Test Agreement",
+            "This is a test agreement",
+            Employee.address,
+            skills,
+            paymentAmount
+        )
+        const agreementCount = await Company.AgreementContract.getAgreementCount();
+        expect(agreementCount).to.equal(1);
     });
 });
